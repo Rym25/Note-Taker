b6a92df50a0b5c930c9e2d8c968a8e5d7239c202
Ryely <ryely.moore@outlook.com>
2021-07-11 13:36:49 -0600

    added get routes for /notes for notes.html and * so that any unknown path will send the index.html

--- server.js
+++ server.js
@@ -18,7 +18,15 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    // sends the html file to the / route
+    // sends the index.html file to the / route
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) => {
+    // sends the notes.html file to the /notes route
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+app.get('*', (req, res) => {
+    // sends the index.html file when an undefined route/url is given
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
