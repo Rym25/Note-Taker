1805631af9490a5ced6f3725cdc27e3d488d9a8c
Ryely <ryely.moore@outlook.com>
2021-07-11 13:32:46 -0600

    added get route to send the index.html file in public when / url is called

--- server.js
+++ server.js
@@ -2,20 +2,24 @@
 const express = require('express');
 // require the json database
 const notes = require('./db/db.json');
+// requires the path library
+const path = require('path');
 
 // creates a conditional value for PORT that will either match the PORT used by the environment of the process that is running the server? or port 3001
 const PORT = process.env.PORT || 3001;
 // sets app to an instance of the express method
 const app = express();
 
+// Instructs the server to make files in provided path static resources
+app.use(express.static('public'));
 // parses incoming string or array data
 app.use(express.urlencoded({extended: true}));
 // parses incoming JSON data
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    let results = notes;
-    res.json(results);
+    // sends the html file to the / route
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
